225d9a7911e8bce774c1cd12d8373694f2c616c8
Igor <ig1015@ic.ac.uk>
2020-02-29 14:26:42 +0000

    boundary subroutine is in place, vorticity at time calculation written, both untested

--- LidDrivenCavity.cpp
+++ LidDrivenCavity.cpp
@@ -43,10 +43,61 @@ void LidDrivenCavity::Initialise()
     memset(v,0,Nx*Ny*sizeof(double));
     s = new double [Nx*Ny];
     memset(s,0,Nx*Ny*sizeof(double));
+    dx = Lx / (Nx-1);
+    dy = Ly / (Ny-1);
 }
 
 void LidDrivenCavity::Integrate()
 {
+    // Calculate vorticity at time t
+    for (int i=1 ; i < Nx-1 ; i++ )         //i index restricted from second to penultimate node 
+    {
+        for (int j=1 ; j<Ny ; j++)
+        {
+            v[i*Ny + j] = (s[(i+1)*Ny+j] - 2 * s[i*Ny + j] + s[(i-1)*Ny+j] ) / (dx * dx) 
+                + ( s[i*Ny + j+1] - 2 * s[i*Ny + j] + s[i*Ny + j+1] ) / (dy *dy);
+        }
+    }
+}
+
+void LidDrivenCavity::Boundary()
+{
+    // top boundary
+    if (neigh[0] == -2)
+    {
+        for (int i = 0; i<Nx; i++)
+        {
+            // assume U=1, I*Ny -> first element in each column (Data is saved column wise)
+            v[i*Ny] = 2/ (dy*dy) * ( s[i*Ny] - s[i*Ny + 1] ) - 2 / dy;
+        }
+    }
+    
+    // right boundary
+    if (neigh[1] == -2)
+    {
+        for (int i = 0 ; i < Ny ; i++)
+        {
+            v[(Nx-1)*Ny+i] = 2 / (dx * dx) * (s[(Nx-1)*Ny+i] - s[(Nx-2)*Ny+i]);
+        }
+    }
+    
+    // bottom boundary
+    if (neigh[2] == -2)
+    {
+        for (int i = 0; i<Nx; i++)
+        {
+            v[i*Ny + (Ny - 1)] = 2 / (dy*dy) * (s[i*Ny + (Ny - 1)] - s[i*Ny + (Ny - 2)]);
+        }
+    }
+    
+    // left boundary
+    if (neigh[3] == -2)
+    {
+        for (int i = 0 ; i < Ny ; i++)
+        {
+            v[i] = 2 / (dx * dx) * (s[i] - s[Ny+i]);
+        }
+    }
 }
 
 double LidDrivenCavity::get_dt()
@@ -80,4 +131,26 @@ void LidDrivenCavity::SetCartesianCoordinates(int y , int x)
 {
     y_c = y;
     x_c = x;
+}
+
+void LidDrivenCavity::SetNeighbours(int *p, int size)
+{
+    // Convention is such that neighbours are defined: North, East, South, West
+    for (int i=0 ; i<size ; i++ )
+    {
+            neigh[i] = p[i];
+    }
+}
+
+void LidDrivenCavity::getNeighbours(int*p)
+{
+   for (int i=0 ; i<4 ; i++ )
+    {
+            p[i] = neigh[i];
+    } 
+}
+
+void LidDrivenCavity::SetRank(int processRank)
+{
+    rank = processRank;
 }
